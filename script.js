ced8ad8a3725ed87c91bfcf5229b7b818bc04eda
beslerpatryk <beslerpatryk@gmail.com>
2022-09-19 18:22:46 +0200

    feat: refactor code to fit new markup, add if statements when no input on preview

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const form = document.querySelector("form")
 const confirmationEl = document.getElementById("confirmation")
 const submitBtn = document.getElementById("submit")
 
-const cardNum = document.getElementById("card-num")
-const cardHolder = document.getElementById("card-holder")
-const monthExpiration = document.getElementById("card-month-expiration")
-const yearExpiration = document.getElementById("card-year-expiration")
-const cvc = document.getElementById("card-cvc")
+const cardNum = document.querySelector(".number-preview")
+const cardHolder = document.querySelector(".name-preview")
+const monthExpiration = document.querySelector(".month-preview")
+const yearExpiration = document.querySelector(".year-preview")
+const cvc = document.querySelector(".cvc-preview")
 
 const cardNumInput = document.getElementById("card-num-input")
 const cardholderInput = document.getElementById("card-name")
@@ -16,25 +16,37 @@ const yearExpirationInput = document.getElementById("year-expiration-input")
 const cvcInput = document.getElementById("card-cvc-input")
 
 
-
 submitBtn.addEventListener('mouseDown', ()=> {
     confirmationEl.style = "display: initial"
 })
 
 cardNumInput.addEventListener('keyup', (e)=>{
     cardNum.innerHTML = cardNumInput.value
+    if(cardNum.innerHTML === ""){
+        cardNum.innerHTML = "0000 0000 0000 0000"
+    }
 })
 
 cardholderInput.addEventListener('keyup', (e)=>{
     cardHolder.innerHTML = cardholderInput.value
+    if(cardHolder.innerHTML === ""){
+        cardHolder.innerHTML = "Jane Appleseed"
+    }
 })
 
 monthExpirationInput.addEventListener('keyup', (e)=>{
-    monthExpiration.innerHTML = monthExpirationInput.value
+    monthExpiration.innerHTML = monthExpirationInput.value + "/"
+    if(monthExpiration.innerHTML === ""){
+        monthExpiration.innerHTML = "00/"
+    }
 })
 
 yearExpirationInput.addEventListener('keyup', (e)=>{
     yearExpiration.innerHTML = yearExpirationInput.value
+    if(yearExpiration.innerHTML === ""){
+        yearExpiration.innerHTML = "00"
+    }
+    
 })
 
 cvcInput.addEventListener('keyup', (e)=>{
@@ -45,7 +57,6 @@ cvcInput.addEventListener('keyup', (e)=>{
 function displayConfirm(){
     form.style = "display: none; opacity: 0%;"
     confirmationEl.style = "display: flex"
-    
 }
 
 function showForm(){
