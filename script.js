1383ae9a8b5a069b728e9b3304c73c57d6a83fb0
beslerpatryk <beslerpatryk@gmail.com>
2022-09-20 11:23:35 +0200

    feat: split card number

--- script.js
+++ script.js
@@ -15,16 +15,37 @@ const monthExpirationInput = document.getElementById("month-expiration-input")
 const yearExpirationInput = document.getElementById("year-expiration-input")
 const cvcInput = document.getElementById("card-cvc-input")
 
+let numOfSpaces = 0
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(messages.length > 0){
+        e.preventDefault()
+    }
+    displayConfirm();
+})
 
 submitBtn.addEventListener('mouseDown', ()=> {
     confirmationEl.style = "display: initial"
 })
 
 cardNumInput.addEventListener('keyup', (e)=>{
+    let cardNumLength = cardNumInput.value.length;
+    cardNumLength = cardNumLength - numOfSpaces;
+
     cardNum.innerHTML = cardNumInput.value
     if(cardNum.innerHTML === ""){
         cardNum.innerHTML = "0000 0000 0000 0000"
+        numOfSpaces = 0;
     }
+    if (cardNumLength % 4 === 0 && cardNumLength < 15){
+        cardNumInput.value = cardNumInput.value + " "
+        cardNum.innerHTML = cardNum.innerHTML + " "
+        numOfSpaces++;
+    }
+
+    console.log(cardNumLength)
+
 })
 
 cardholderInput.addEventListener('keyup', (e)=>{
@@ -36,9 +57,16 @@ cardholderInput.addEventListener('keyup', (e)=>{
 
 monthExpirationInput.addEventListener('keyup', (e)=>{
     monthExpiration.innerHTML = monthExpirationInput.value + "/"
-    if(monthExpiration.innerHTML === ""){
+    if(monthExpiration.innerHTML === "/"){
         monthExpiration.innerHTML = "00/"
+    } else if (monthExpirationInput.value > 12){
+        monthExpiration.innerHTML = 12;
+        monthExpirationInput.value = 12;
+    } else if (monthExpirationInput.value <= 0){
+        monthExpiration.innerHTML = 1 + "/"
+        monthExpirationInput.value = 1 
     }
+
 })
 
 yearExpirationInput.addEventListener('keyup', (e)=>{
@@ -46,13 +74,14 @@ yearExpirationInput.addEventListener('keyup', (e)=>{
     if(yearExpiration.innerHTML === ""){
         yearExpiration.innerHTML = "00"
     }
-    
 })
 
 cvcInput.addEventListener('keyup', (e)=>{
     cvc.innerHTML = cvcInput.value
+    if(cvc.innerHTML === ""){
+        cvc.innerHTML = "000"
+    }
 })
-
 
 function displayConfirm(){
     form.style = "display: none; opacity: 0%;"
